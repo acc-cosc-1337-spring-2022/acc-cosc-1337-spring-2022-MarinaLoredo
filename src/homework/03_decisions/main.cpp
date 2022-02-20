28415e67802550ccf6ddeb8e786828981849f817
Mars Loredo <93014911+MarinaLoredo@users.noreply.github.com>
2022-02-19 23:08:44 -0600

    Revert "hw03 and tests"

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -10,28 +10,12 @@ int main()
 	cout << "\nEnter numerical grade: ";
 	cin >> grade;
 
-	if(grade < 101 && grade > 0)
-	{
-		string letter_if = get_letter_grade_using_if(grade);
-		cout << "Letter grade based on 'if' function: " << letter_if;
-	}
-	else
-	{
-		cout << "That grade is not valid";
-	}
+	string letter_if = get_letter_grade_using_if(grade);
+
+	cout << "\nLetter grade based on 'if' function: " << letter_if;
 	
-	cout << "\nEnter numerical grade: ";
-	cin >> grade;
-	
-	if(!(grade < 0 || grade > 100))
-	{
-		string letter_switch = get_letter_grade_using_switch(grade);
-		cout << "\nLetter grade based on 'switch' function: " << letter_switch;
-	}
-	else
-	{
-		cout << "That grade is not valid";
-	}	
+	string letter_switch = get_letter_grade_using_switch(grade);
+	cout << "\nLetter grade based on 'switch' function: " << letter_switch;
 
 	return 0;
 }
