601767bdd76c93e3dc8995c7dd9b10af788926ea
mars-loredo <rachael.loredo@g.austincc.edu>
2022-02-26 23:16:04 -0600

    hw04 and tests

--- src/homework/04_iteration/dna.h
+++ src/homework/04_iteration/dna.h
@@ -1,19 +1,12 @@
 #include<string>
 
-/*
-Write prototype for function get_gc_content that accepts
-a const reference string parameter and returns a double
-*/
+void menu_options();
 
+void menu();
 
-/*
-Write prototype for function get_reverse_string that 
-accepts a string parameter and returns a string
-*/
+int handle_menu(int menu_selection);
 
+int factorial(int num);
 
-/*
-Write prototype for function get_dna_complement that
-accepts a string and returns a string
-*/
+int gcd(int num1, int num2);
 
