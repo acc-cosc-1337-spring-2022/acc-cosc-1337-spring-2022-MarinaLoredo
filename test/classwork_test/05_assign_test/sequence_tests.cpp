e5cd9a7892dbbe0498412000e43d6e8b8e69bf3d
mars-loredo <rachael.loredo@g.austincc.edu>
2022-03-05 21:34:16 -0600

    commit because

--- test/classwork_test/05_assign_test/sequence_tests.cpp
+++ test/classwork_test/05_assign_test/sequence_tests.cpp
@@ -1,8 +1,23 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "loops.h"
+#include "sequence.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Verify get_gc_content") {
+	REQUIRE(get_gc_content("AGCTATAG") == .375);
+	REQUIRE(get_gc_content("CGCTATAG") == .50);
+}
+
+TEST_CASE("Verify get_dna_compliment") {
+	REQUIRE(get_dna_compliment("AAAACCCGGT") == "ACCGGGTTTT");
+	REQUIRE(get_dna_compliment("CCCGGAAAAT") == "ATTTTCCGGG");
+}
+
+TEST_CASE("Verify reverse_string") {
+	REQUIRE(reverse_string("AGCTATAG") == "GATATCGA");
+	REQUIRE(reverse_string("CGCTATAG") == "GATATCGC");
+}
